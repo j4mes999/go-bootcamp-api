3363ecc6bdde95ac48bdbf33b6319ab68290148f
Luis Gonzalez Quinchanegua <luis.gq@Luiss-MacBook-Pro.local>
2022-05-19 17:02:26 -0500

    Add new functionality to use modules and http codes

--- main.go
+++ main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-	//"net/http"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
+		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run() // listen and serve on 0.0.0.0:8080 (for win´dows "localhost:8080")
 }
